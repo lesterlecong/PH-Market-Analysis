5fa59829c0fc6939d4a56e18b01fe444a5e90847
Lester Lecong <lester_lecong@outlook.com>
2019-01-14 22:40:35 +0800

    Accessing 10 year bond yield.

--- Market_Analysis.py
+++ Market_Analysis.py
@@ -16,14 +16,23 @@ def get_ph_25_year_bond_yield():
     df.drop(['Open', 'High', 'Low', 'Change %'], axis=1, inplace=True)
     return df
 
+def get_ph_10_year_bond_yield():
+    df = pd.read_csv('Data/Philippines 10-Year Bond Yield Historical Data.csv')
+    df.set_index('Date', inplace=True)
+    df.drop(['Open', 'High', 'Low', 'Change %'], axis=1, inplace=True)
+    return df
+
 fig = plt.figure()
 ax1 = plt.subplot2grid((2,1), (0,0))
 
+ph_10_yr_bond = get_ph_10_year_bond_yield()
 ph_20_yr_bond = get_ph_20_year_bond_yield()
 ph_25_yr_bond = get_ph_25_year_bond_yield()
 
+ph_10_yr_bond['Price'].plot(ax = ax1, label='10 yr bond yield')
 ph_20_yr_bond['Price'].plot(ax = ax1, label='20 yr bond yield')
 ph_25_yr_bond['Price'].plot(ax = ax1, label='25 yr bond yield')
+
 ax1.legend(loc=4)
 plt.legend(loc=4)
 plt.show()
