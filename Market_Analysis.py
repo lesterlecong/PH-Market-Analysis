931e2fafd39fad15c3274d849d322633d621ff66
Lester Lecong <lester_lecong@outlook.com>
2019-01-14 22:22:58 +0800

    Initial view of 20 year bond yield in matplotlib.

--- Market_Analysis.py
+++ Market_Analysis.py
@@ -1,5 +1,14 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('fivethirtyeight')
 
 df = pd.read_csv('Data/Philippines 20-Year Bond Yield Historical Data.csv')
+df.set_index('Date', inplace=True)
 df.drop(['Open', 'High', 'Low', 'Change %'], axis=1, inplace=True)
 print(df.head())
+
+df.plot()
+plt.legend(loc=4)
+plt.show()
